4643136dcf0927673528173922beb547f3664b36
Ulyanov Nikita <batyaultr03@gmail.com>
2025-04-14 17:35:11 +0400

    First commit

---
+++ src/test/java/com/example/neoflextesttask/NeoflexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neoflextesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
